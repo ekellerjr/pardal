f201a2404f0e08b8deffbb9a88f94a8296f37d13
Eugen <eugen.keller@stud.uni-due.de>
2019-02-03 12:12:00 +0100

    + added log and env functionality

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+extern crate log;
+extern crate env_logger;
+extern crate dotenv;
+
+use log::{debug, error, info};
+
+use dotenv::dotenv;
+
+fn main() {
+	init_env();
+	init_log();
+}
+
+fn init_log() {
+	env_logger::init();
+	info! {"logger initialised"};
+}
+
+fn init_env() {
+	dotenv().ok();
+}
+
